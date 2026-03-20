8e9125ef94e334b1c3d677544d7eb2b8ac91fcb9
JAYESH SINGH <jayeshsingh971@gmail.com>
2026-03-21 01:31:07 +0530

    new Update

--- wpa/user/urls.py
+++ wpa/user/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path("" , views.home , name = 'home'),
+    path("about/" , views.about , name = 'about'),
+    path("contact/" , views.contact , name = 'contact'),
+    path("register/" , views.register , name = 'register'),
+    path("login/" , views.login , name = 'login'),
 ]
 
--- wpa/user/views.py
+++ wpa/user/views.py
@@ -1,6 +1,20 @@
+from urllib import request
+
 from django.shortcuts import render , HttpResponse
 from .models import *
 # Create your views here.
 
 def home(request):
-    return render(request , "navbar.html")
+    return render(request , "home.html")
+
+def about(request):
+    return render(request , "about.html")
+
+def contact(request):
+    return render(request , "contact.html")
+
+def login(request):
+    return render(request , "login.html")
+
+def register(request):
+    return render(request , "registration.html")
